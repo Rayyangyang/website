2837fb26b95fa21a69baff6fc4c53f4c8b52eac3
rayyangyang <2515762599@qq.com>
2023-06-05 10:52:34 +0800

    init

--- src/views/SuccessCase.vue
+++ src/views/SuccessCase.vue
@@ -11,15 +11,18 @@
         </div>
       </div>
       <div class="right-content">
-        <div v-for="ele in contents" :key="ele.title">
-          <div class="img">
-            <img :src="ele.img" alt="" />
-          </div>
-          <div class="content">
-            <h2>{{ ele.title }}</h2>
-            <p>{{ ele.desc }}</p>
+        <div v-if="contents[curI].content.length > 0">
+          <div v-for="ele in contents[curI].content" :key="ele.title">
+            <div class="img">
+              <img :src="ele.img" alt="" />
+            </div>
+            <div class="content">
+              <h2>{{ ele.title }}</h2>
+              <p>{{ ele.desc }}</p>
+            </div>
           </div>
         </div>
+        <div v-else class="no-data">暂无数据</div>
       </div>
     </div>
 
@@ -32,6 +35,10 @@ import { ref, onMounted } from "vue"
 import Footer from "./Footer.vue"
 
 import pi4 from "../assets/imgs/pi4.png"
+import successImg1 from "../assets/imgs/successImg1.png"
+import successImg2 from "../assets/imgs/successImg2.png"
+import successImg3 from "../assets/imgs/successImg3.png"
+import successImg4 from "../assets/imgs/successImg4.png"
 
 onMounted(() => {
   document.documentElement.scrollTop = 0
@@ -42,14 +49,45 @@ onMounted(() => {
 const navLis = ref(["企业定制酒", "地域文化酒", "品牌联名酒", "思域圈层酒", "行业定制酒"])
 const contents = ref([
   {
-    title: "蒲丹「洲河醉」联合创始人",
-    img: pi4,
-    desc: "曾经我站在青藏高原守卫祖国边疆，退伍转业后是洲河醉让我找到了人生的第二个奋斗目标，感谢公司专业贴心的服务让我感受到做酒原来如此简单，从一位白酒爱好者到资深合伙人，只用了一年的时间，并确立了自己的核心市场，颇有收效。"
+    title: "企业定制酒-中南海运",
+    content: [
+      {
+        title: "企业定制酒-中南海运",
+        img: successImg1,
+        desc: "作为全球最大的集装箱码头运营商，中国远洋海运集团完善的全球化服务筑就了网络服务优势与品牌优势。集团集采平台与泸南酒业合作开发“中国泸州特曲酒”酒体优质，设计简洁大方，备受集团成员青睐。"
+      },
+      {
+        title: "蒲丹「洲河醉」联合创始人",
+        img: successImg2,
+        desc: "曾经我站在青藏高原守卫祖国边疆，退伍转业后是洲河醉让我找到了人生的第二个奋斗目标，感谢公司专业贴心的服务让我感受到做酒原来如此简单，从一位白酒爱好者到资深合伙人，只用了一年的时间，并确立了自己的核心市场，颇有收效。"
+      },
+      {
+        title: "成都康弘药业集团股份有限公司",
+        img: successImg3,
+        desc: "贵司通过强大的供应链保障能力，满足了集团的用酒需求，特别是专业的设计让公司的整体形象，让我们在日常接待及赠礼过程中提升了品牌形象，同时小批量销售也为员工和公司创造了价值，双方在未来将继续深入合作。"
+      },
+      {
+        title: "中国农垦控股",
+        img: successImg4,
+        desc: "作为央企，我们强调服务社会，造福人民的理念，与泸南销售公司联名开发“中垦飨味堂-待宾酒”是我们“粮安天下，福慧民生”的品牌愿景，完成了将中国白酒联名共赢模式推上新高峰的整体战略布局。"
+      }
+    ]
   },
   {
-    title: "成都康弘药业集团股份有限公司",
-    img: pi4,
-    desc: "贵司通过强大的供应链保障能力，满足了集团的用酒需求，特别是专业的设计让公司的整体形象，让我们在日常接待及赠礼过程中提升了品牌形象，同时小批量销售也为员工和公司创造了价值，双方在未来将继续深入合作。"
+    title: "地域文化酒",
+    content: []
+  },
+  {
+    title: "品牌联名酒",
+    content: []
+  },
+  {
+    title: "思域圈层酒",
+    content: []
+  },
+  {
+    title: "行业定制酒",
+    content: []
   }
 ])
 
@@ -107,5 +145,12 @@ const curI = ref(0)
       }
     }
   }
+}
+
+.no-data {
+  border: none !important;
+  font-size: 40px;
+  text-align: center;
+  line-height: 200px;
 }
 </style>
